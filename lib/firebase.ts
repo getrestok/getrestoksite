b35db344be364c532538dbea06653cec3b32b469
corytest <corkyhold@gmail.com>
2025-12-10 15:15:35 -0500

    Add firebase stuff

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyACvC_hSZUaqEDh1r9ohjUS2fZhr0iJXqY",
+  authDomain: "stockpilot-e2a28.firebaseapp.com",
+  projectId: "stockpilot-e2a28",
+  storageBucket: "stockpilot-e2a28.firebasestorage.app",
+  messagingSenderId: "1067461917580",
+  appId: "1:1067461917580:web:e70b5f8b465d4182e1f4a9",
+  measurementId: "G-L0PWL48295"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
